4f3a3570a2a8ddba55d7b9190cd2f83ffb1094d7
Nicholas Siew <nicholas_siew1991@hotmail.com>
2015-11-30 20:25:54 +0800

    .

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -1,3 +1,5 @@
+
+
 public abstract class ChessGame {
 
 	private String strPlayerTurn = null;
@@ -8,23 +10,5 @@ public abstract class ChessGame {
 
 	public void setPlayerTurn(String strPlayerTurn) {
 		this.strPlayerTurn = strPlayerTurn;
-	}
-}
-
-class ChineseDarkChessGame extends ChessGame {
-  
-  public static final int SIDE_RED = 1;
-  public static final int SIDE_BLACK = 2;
-
-	private Player p1;
-	private Player p2;
-
-	public void setPlayer(Player p1, Player p2) {
-		this.p1 = p1;
-		this.p2 = p2;
-	}
-
-	private void generateChesses() {
-
 	}
 }
