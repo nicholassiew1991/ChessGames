c63f105553b335070c83be1fd410bb95aa5710ee
Nicholas Siew <nicholas_siew1991@hotmail.com>
2015-12-12 10:34:55 +0800

    12/12/2015 10:34 AM

---
+++ src/ChessBoard.java
@@ -0,0 +1,17 @@
+public class ChessBoard
+{
+       private int LocX, LocY;
+       public void setLocation(int x, int y)
+       {
+              this.LocX = x;
+              this.LocY = y;
+       }
+       public int getLocationX()
+       {
+              return this.LocX;
+       }
+       public int getLocationY()
+       {
+              return this.LocY;
+       }
+}
---
+++ src/DarkChessBoard.java
@@ -0,0 +1,4 @@
+public class DarkChessBoard extends ChessBoard
+{
+       //
+}
