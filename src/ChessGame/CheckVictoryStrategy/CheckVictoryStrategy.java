d9a5e76bdac9d9fe00d3defd52b6b1550fe7fe2a
Nicholas Siew <nicholas_siew1991@hotmail.com>
2015-12-31 02:02:23 +0800

    Delete the strategies.

--- src/ChessGame/CheckVictoryStrategy/CheckVictoryStrategy.java
+++
@@ -1,7 +0,0 @@
-package ChessGame.CheckVictoryStrategy;
-
-import ChessBoard.ChessBoard;
-
-public interface CheckVictoryStrategy<T extends ChessBoard> {
-  boolean checkVitory(T cb, int x, int y);
-}
